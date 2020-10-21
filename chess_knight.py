e0cb5f9476293ace756405d218aa9d93b2c5d658
theophaneDurand <durandt@hotmail.fr>
2020-10-22 01:30:46 +0200

    Fin de la phase 1

--- chess_knight.py
+++ chess_knight.py
@@ -6,19 +6,27 @@ def chess_knight(start, nb_moves):
     [x, y] = cell2coord(start)
     # Create the list of next positions : OK
     # Reduce the list to valid position : OK
-    reachablePos = nextpos(x, y)
+    reachableCoord = nextpos(x, y)
+    # Change coord into cell
+    reachableCells = [coord2cell(coord) for coord in reachableCoord]
+    # Sorting of the list
+    reachableCells.sort()
     # retrun results
-
-    return reachablePos
+    return reachableCells
 
 def cell2coord(start):
     lettre2num = {"a" : 1, "b" : 2, "c" : 3, "d" : 4, "e" : 5, "f" : 6, "g" : 7, "h" : 8}
-    return[lettre2num[start[0].lower()], start[1]]
+    return[lettre2num[start[0].lower()], int(start[1])]
     
 def nextpos(x, y):
     # On calcule les positions atteignables depuis la position actuelle :
     reachablePos = list(product([x-1, x+1],[y-2, y+2])) + list(product([x-2,x+2],[y-1,y+1]))
     # On restreint les positions trouvées au positions réelles :
-    reachablePos = [(x,y) for x,y in reachablePos if x >= 0 and y >= 0 and x < 8 and y < 8]
+    reachablePos = [(x,y) for x,y in reachablePos if x > 0 and y > 0 and x <= 8 and y <= 8]
     # On renvoit le résultat
     return reachablePos
+
+def coord2cell(coord):
+    lettre = ["", "a", "b", "c", "d", "e", "f",  "g", "h"]
+    cell = lettre[coord[0]]+str(coord[1])
+    return cell
